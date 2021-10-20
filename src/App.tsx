80f1a8ea44494fb2959bba9be1f5fa2e099f5eb7
Alpha <alphatero@icloud.com>
2021-10-20 23:18:23 +0800

    add backgroundimage and add click search button into result page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React, { useState } from 'react'
-import { Route, Switch } from 'react-router-dom';
+import React, { ReactNode, useState } from 'react'
+import { Route, Switch, useHistory } from 'react-router-dom';
+
+function TextField() {
+  return (
+
+    <input className="border border-gray-200 text-lg rounded outline-none px-2 py-1" type="text" name="search" id="search" />
+  )
+}
+
+// function Search
 
 function Header() {
+  const history = useHistory();
+
+  const clickSearch = () => {
+    history.push("/result")
+  }
   return (
     <header className="flex flex-col justify-center items-center gap-4 pt-10">
       <h2>Where would you like to go today</h2>
       <div className="flex flex-col gap-4">
-
-        <input className="border border-gray-200 text-lg rounded outline-none px-2 py-1" type="text" name="search" id="search" />
-        <button className="bg-green-400 py-2">Search</button>
+        <TextField />
+        <button className="bg-green-400 py-2" onClick={clickSearch}>Search</button>
       </div>
     </header>
   );
@@ -16,7 +29,7 @@ function Header() {
 
 function TrySearch() {
   return (
-    <section className="w-full h-screen bg-white p-4 flex flex-col gap-3">
+    <section className="w-full h-screen  p-4 flex flex-col gap-3">
       <h2 className="text-xl">Try Searching</h2>
       <p>Bus station nearby me</p>
       <ul className="flex flex-col gap-2">
@@ -34,6 +47,14 @@ function TrySearch() {
   )
 }
 
+function Result() {
+  return (
+    <div>
+      <h2>Result</h2>
+    </div>
+  )
+}
+
 
 function App() {
 
@@ -43,6 +64,9 @@ function App() {
       <Switch>
         <Route path='/' exact>
           <TrySearch />
+        </Route>
+        <Route path='/result' exact>
+          <Result />
         </Route>
       </Switch>
     </main>
