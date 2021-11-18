df71ba71ac2d2f5c3d719ddf6afcfc20df0260dd
kayac-chang <egg734631@gmail.com>
2021-11-18 17:38:31 +0800

    add routes tabs

--- src/pages/routes/index.tsx
+++ src/pages/routes/index.tsx
@@ -1,42 +1,44 @@
 import clsx from "clsx";
+import { useLocation, Link, To } from "react-router-dom";
+import { cond, equals, T } from "ramda";
+import { ReactNode } from "react";
 
 import { Icon, Tabs } from "@/components";
+import { HasID, HasName } from "@/models";
 import Arrival from "./Arrival";
 
-function PageTabs() {
+interface Tab extends HasID, HasName {
+  icon: ReactNode;
+  active?: boolean;
+  to: To;
+}
+type PageTabsProps = {
+  items: Tab[];
+};
+function PageTabs({ items }: PageTabsProps) {
   return (
     <div className="flex flex-col relative overflow-hidden pt-2">
-      <Tabs
-        classes={{ list: "flex", item: "flex-1" }}
-        items={[
-          {
-            id: "1",
-            name: "公車路線",
-            icon: <Icon.Route className="w-9" />,
-            active: true,
-          },
-          { id: "2", name: "公車地圖", icon: <Icon.Map className="w-10" /> },
-          { id: "3", name: "公車資訊", icon: <Icon.Info className="w-8" /> },
-        ]}
-      >
-        {({ name, icon, active }) => (
-          <div
-            className={clsx(
-              "h-full py-3 rounded-2xl",
-              "flex flex-col justify-end items-center gap-1",
-              active
-                ? "shadow bg-white text-orange relative z-10"
-                : "text-gray-400"
-            )}
-          >
-            {icon}
+      <Tabs classes={{ list: "flex", item: "flex-1" }} items={items}>
+        {({ name, icon, active, to }) => (
+          <Link to={to}>
+            <div
+              className={clsx(
+                "h-full py-3 rounded-2xl",
+                "flex flex-col justify-end items-center gap-1",
+                active
+                  ? "shadow bg-white text-orange relative z-10"
+                  : "text-gray-400"
+              )}
+            >
+              {icon}
 
-            <strong>{name}</strong>
+              <strong>{name}</strong>
 
-            {active && (
-              <div className="bg-white h-3 w-full absolute bottom-0" />
-            )}
-          </div>
+              {active && (
+                <div className="bg-white h-3 w-full absolute bottom-0" />
+              )}
+            </div>
+          </Link>
         )}
       </Tabs>
 
@@ -46,12 +48,42 @@ function PageTabs() {
 }
 
 export function Routes() {
+  const location = useLocation();
+
   return (
     <div className="flex flex-col flex-1">
-      <PageTabs />
+      <PageTabs
+        items={[
+          {
+            id: "1",
+            name: "公車路線",
+            icon: <Icon.Route className="w-9" />,
+            to: { ...location, hash: undefined },
+            active: !location.hash,
+          },
+          {
+            id: "2",
+            name: "公車地圖",
+            icon: <Icon.Map className="w-10" />,
+            to: { ...location, hash: "#map" },
+            active: location.hash === "#map",
+          },
+          {
+            id: "3",
+            name: "公車資訊",
+            icon: <Icon.Info className="w-8" />,
+            to: { ...location, hash: "#info" },
+            active: location.hash === "#info",
+          },
+        ]}
+      />
 
       <div className="flex-1">
-        <Arrival />
+        {cond<string, ReactNode>([
+          [equals("#map"), () => <></>],
+          [equals("#info"), () => <></>],
+          [T, () => <Arrival />],
+        ])(location.hash)}
       </div>
     </div>
   );
