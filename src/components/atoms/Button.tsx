dc182cc6c0598e6fb3f4754d00b3c4f05a19f55d
kayac-chang <egg734631@gmail.com>
2021-11-24 12:59:44 +0800

    fix button to style component

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,21 +1,22 @@
 import clsx from "clsx";
-import { ReactNode } from "react";
+import { cloneElement, isValidElement, ReactNode } from "react";
+import invariant from "tiny-invariant";
 
 type Props = {
   children?: ReactNode;
   disabled?: boolean;
 };
-export function Button(props: Props) {
-  return (
-    <button
-      className={clsx(
-        "bg-dark-green text-white rounded-full",
-        "py-2 text-lg font-bold tracking-widest",
-        "transition-all duration-300 ease-out-circ",
-        "active:bg-blue",
-        "disabled:opacity-50"
-      )}
-      {...props}
-    />
-  );
+export function Button({ children, disabled }: Props) {
+  invariant(isValidElement(children), "should be valid react element");
+
+  return cloneElement(children, {
+    className: clsx(
+      "bg-dark-green text-white text-center rounded-full",
+      "py-2 text-lg font-bold tracking-widest",
+      "transition-all duration-300 ease-out-circ",
+      "active:bg-blue",
+      "disabled:opacity-50"
+    ),
+    disabled,
+  });
 }
