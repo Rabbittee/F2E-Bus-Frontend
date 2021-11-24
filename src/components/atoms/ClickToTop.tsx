b2ec7e03b97e0ee000f695c0276538ffe31def59
Alpha <alphatero@icloud.com>
2021-11-25 00:21:33 +0800

    add some function to visable button

--- src/components/atoms/ClickToTop.tsx
+++ src/components/atoms/ClickToTop.tsx
@@ -1,9 +1,35 @@
 import { Icon } from "@/components";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
 
 export function ClickToTopButton() {
+  const [isVisable, setIsVisable] = useState<Boolean>(false);
+
+  const toggleVisibility = () => {
+    scrollY > 300 ? setIsVisable(true) : setIsVisable(false);
+  };
+
+  const scrollToTop = () => {
+    scroll({
+      top: 0,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.addEventListener("scroll", toggleVisibility);
+    };
+  }, []);
   return (
-    <div className="fixed w-12 bottom-3 right-3 lg:hidden z-10">
-      <a href="#root" className="text-light-blue opacity-40">
+    <div
+      className={clsx(
+        "fixed w-12 bottom-3 right-3 lg:hidden z-10 duration-300 transition-all",
+        !isVisable && "pointer-events-none opacity-0"
+      )}
+    >
+      <a onClick={scrollToTop} className="text-light-blue opacity-40">
         <Icon.ClickToTop />
       </a>
     </div>
