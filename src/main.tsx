bba881b24c9833d72b13ed97ac5b980b78c4e317
kayac-chang <egg734631@gmail.com>
2021-11-27 00:36:26 +0800

    remove unuse mock

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { StoreProvider } from "@/logic";
-import { MODE_DEV_MOCK } from "@/config";
 
-async function main() {
-  if (MODE_DEV_MOCK) {
-    (await import("@/mock")).default();
-  }
-
-  ReactDOM.render(
-    <StrictMode>
-      <StoreProvider>
-        <App />
-      </StoreProvider>
-    </StrictMode>,
-    document.getElementById("root")
-  );
-}
-
-main();
+ReactDOM.render(
+  <StrictMode>
+    <StoreProvider>
+      <App />
+    </StoreProvider>
+  </StrictMode>,
+  document.getElementById("root")
+);
